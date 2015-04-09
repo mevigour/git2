14271a9823ae7cbb207d8428ab38d4917066b72c
mevigour <mevigour@gmail.com>
2015-04-09 17:05:10 +0800

    2

---
+++ test1.py
@@ -0,0 +1,14 @@
+import time
+import os
+import datetime
+import random
+
+s1 = random.randint(10, 100)
+print(s1)
+
+
+hour = time.localtime().tm_hour
+
+print(hour)
+s3 = random.randint(1, 10)
+print(s3)
